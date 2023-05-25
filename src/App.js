bb811ff94460d74087d6dfe10ae3fc0b12bd39d9
MauroGomez1994 <123425530+MauroGomez1994@users.noreply.github.com>
2023-05-24 23:27:02 -0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import NavBar from './NavBar';
+
+const App = () => {
+  return (
+    <div>
+      <NavBar />
+      <h1>Bienvenido a nuestra tienda online</h1>
+      <p>¡Explora nuestros productos y realiza tus compras!</p>
+    </div>
+  );
+};
+
+export default App;
---
+++ src/NavBar.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+const NavBar = () => {
+  return (
+    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+      <div className="container">
+        <a className="navbar-brand" href="/">LA ALIANZA S.A</a>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-toggle="collapse"
+          data-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul className="navbar-nav ml-auto">
+            <li className="nav-item">
+              <a className="nav-link" href="/">Inicio</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="/productos">Productos</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="/carrito">Contacto</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="/contacto">Carrito</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default NavBar;
+
